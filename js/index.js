7bb6b6856aa2c5fcb20b8d2b04a647249ce25ca1
808Aidan <alyum22@punahou.edu>
2016-06-29 11:55:05 -1000

    Got an idea for the app.
    We are punching Justin Beiber

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-var arr = ['red']
+var colors = [
+"red"
+"orange"
+"yellow"
+"green"
+"blue"
+"indigo"
+"violent"
+];
+
+var count = 0;
+
+var cycle = function[array] {
+	console.log(array[count]);
+	if (count == array. length-1) {
+		count = 0;
+	} else {
+		count++;
+	};
+};
+
+var display = function(){
+	document.body.style.color = colors[count];
+	cycle(colors);
+
+}
+
+var listall = function(array) {
+	for (var i = 0;  i < array.length; i++) {
+		console.log(i)
+		document.getElementById('colorspace').innerhtml += "<li>" + array[i] + "</li";
+	}
+}
+
+listall(colors)
